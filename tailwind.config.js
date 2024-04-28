689063fa39e208410640b6c23c740c36f0616d01
AtiyehHoshanfar <atiyeh.hoshanfar@gamil.com>
2024-04-28 10:28:43 +0330

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        redPrimary: {
+          100: "#F7B4BB",
+          300: "#F46D74",
+          500: "#BB0102",
+        },
+        grayPrimary: {
+          100: "#E9E9E9",
+          200: "#D4D4D4",
+          300: "#9A9A9A",
+          400: "#707070",
+          500: "#454545",
+          600: "#1A171B",
+        },
+      },
+      fontFamily:{
+        'sans':['"Roboto"',...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+};
